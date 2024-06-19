eb03e788c9e5ac50b7f214bb8e76c24bbbf06691
Nick Meinhold <nick@enspyr.co>
2024-06-19 11:54:43 +1000

    Retrieve query strings

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@ use warp::Filter;
 use serde::{Serialize, Deserialize};
 use std::env;
 
+#[derive(Deserialize, Serialize)]
+struct QueryParams {
+    name: String,
+    identity: String,
+}
+
 #[tokio::main]
 async fn main() {
     // Define the route
     let create_token_route = warp::path("create-token")
-        .map(|| {
-            let token = create_token().unwrap();
+        .and(warp::query::<QueryParams>())
+        .map(|p: QueryParams| {
+            let token = create_token(&p.identity, &p.name, String::from("room") ).unwrap();
             warp::reply::json(&TokenResponse { token })
         });
 
@@ -17,16 +24,16 @@ async fn main() {
 }
 
 // Token creation function
-fn create_token() -> Result<String, access_token::AccessTokenError> {
+fn create_token(identity : &str, name : &str, room : String) -> Result<String, access_token::AccessTokenError> {
    let api_key = env::var("LIVEKIT_API_KEY").expect("LIVEKIT_API_KEY is not set");
    let api_secret = env::var("LIVEKIT_API_SECRET").expect("LIVEKIT_API_SECRET is not set");
 
    let token = access_token::AccessToken::with_api_key(&api_key, &api_secret)
-      .with_identity("identity")
-      .with_name("name")
+      .with_identity(identity)
+      .with_name(name)
       .with_grants(access_token::VideoGrants {
          room_join: true,
-         room: "my-room".to_string(),
+         room: room.to_string(),
          ..Default::default()
       })
       .to_jwt();
